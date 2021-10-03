f7641e9e6b42467346d621089df1c766035f0666
Nathan Mercês <n.galvao@comunique-se.com.br>
2021-10-03 13:17:17 -0300

    Commit inicial - Criando arquivos do projeto

---
+++ src/main/java/com/expertostech/api/rest/java/ApiRestExpertostechApplication.java
@@ -0,0 +1,13 @@
+package com.expertostech.api.rest.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestExpertostechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestExpertostechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expertostech/api/rest/java/ApiRestExpertostechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertostech.api.rest.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestExpertostechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
